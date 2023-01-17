9f3eaaf1f85de08bc9d60c8be3fc50e0f86d7ceb
Daria Ziobrowska <darianocon@gmail.com>
2023-01-17 14:59:49 +0100

    Add constructor method to App Component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,21 +1,37 @@
+import React from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-        <div className="App">
-            <SearchBar />
-            <div className="App-playlist">
-              <SearchResults />
-              <Playlist />
-            </div>
-        </div>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {
+          name: 'name', 
+          artist: 'artist', 
+          album: 'album', 
+          id: 'id'
+        }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+          <div className="App">
+              <SearchBar />
+              <div className="App-playlist">
+                <SearchResults searchResults={this.state.searchResults} />
+                <Playlist />
+              </div>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
